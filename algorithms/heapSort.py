98bac64802abb6c6262b3900ddcf3480b0b78b67
Eric106 <ercv106@hotmail.com>
2018-11-15 20:54:53 -0600

    heapSort

--- algorithms/heapSort.py
+++ algorithms/heapSort.py
@@ -1,2 +1,42 @@
+import time
+intercambios=0
+comparaciones=0
+
+def heapify(arreglo,n,i):
+	global comparaciones
+	global intercambios
+	mas_grande=i
+	l=2*i+1
+	r=2*i+2
+	if l<n and arreglo[i]<arreglo[l]:
+		comparaciones+=1
+		mas_grande=l
+	if r<n and arreglo[mas_grande]<arreglo[r]:
+		comparaciones+=1
+		mas_grande=r
+	if mas_grande!=i:
+		arreglo[i],arreglo[mas_grande]=arreglo[mas_grande],arreglo[i]
+		intercambios+=1
+		heapify(arreglo,i,0)
+def sort(arreglo):
+	global intercambios
+	n=len(arreglo)
+	for i in range(n,-1,-1):
+		heapify(arreglo,n,i)
+	for i in range(n-1,0,-1):
+		arreglo[i],arreglo[0]=arreglo[0],arreglo[i]
+		intercambios+=1
+		heapify(arreglo,i,0)
+
 def heapSort(alist):
-    return 0
+	global comparaciones
+	global intercambios
+	start=time.clock()
+	sort(alist)
+	end=time.clock()
+	print("Tiempo", end-start)
+	print("Comparaciones",comparaciones)
+	print("Intercambios",intercambios)
+	return alist
+
+#print(heapSort([ 12, 11, 13, 5, 6, 7]))
