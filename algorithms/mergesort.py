6b2e34c64e7b88af3416c6a638005e3f3939190a
Hector David Hernandez Rodriguez <hd170998@hotmail.com>
2018-11-19 15:12:23 -0600

    Ultimos cambios

--- algorithms/mergesort.py
+++ algorithms/mergesort.py
@@ -1,62 +1,54 @@
-from algorithms.utilidades import imprimeTablaComplejidad
-
 import time
+intercambios=0
+comparaciones=0
 
 
-def mergeSortHelper(alist, comparaciones, intercambios):
-    contador = 0
+def mergeSortHelper(alist):
+    global intercambios
+    global comparaciones
     if len(alist) > 1:
         middle = len(alist) // 2
         left = alist[:middle]
         rigth = alist[middle:]
-        mergeSortHelper(left, comparaciones, intercambios)
-        mergeSortHelper(rigth, comparaciones, intercambios)
+        mergeSortHelper(left)
+        mergeSortHelper(rigth)
         i = 0
         j = 0
         k = 0
         while i < len(left) and j < len(rigth):
+            print (alist)
             if left[i] < rigth[j]:
                 comparaciones += 1
                 alist[k] = left[i]
                 i += 1
             else:
-                comparaciones += 1
+                comparaciones+=1
                 alist[k] = rigth[j]
                 j += 1
             k += 1
         while i < len(left):
-            intercambios += 1
+            intercambios+=1
             alist[k] = left[i]
             i += 1
             k += 1
-            contador += 1
-            print("Iteración", contador, ":", alist)
+            print(alist)
         while j < len(rigth):
-            intercambios += 1
+            intercambios+=1
             alist[k] = rigth[j]
             j += 1
             k += 1
-            contador += 1
-            print("Iteración", contador, ":", alist)
-    return [alist, comparaciones, intercambios]
+            print(alist)
+    return [alist, comparaciones,intercambios]
 
 
 def mergeSort(alist):
     start = time.time()
-    comparaciones = 0
-    intercambios = 0
-    values = mergeSortHelper(alist, comparaciones, intercambios)
+    global comparaciones
+    global intercambios
+    values = mergeSortHelper(alist)
     end = time.time()
     elapsedtime = end - start
-
-    comparaciones = [f"n²={len(alist)**2}",
-                     f"((n-1)n)/2 = {((len(alist)-1)*len(alist))/2}"]
-    desplazamientos = [
-        f"n²={len(alist)**2}", f"de 0 a ((n-1)n)/2 = {[0,((len(alist)-1)*len(alist))/2]}"]
-
-    realizadas = [values[1], values[2]]
-    imprimeTablaComplejidad(
-        "INTERCAMBIOS", comparaciones, desplazamientos, realizadas, elapsedtime)
-
-
+    print("Tiempo: ", elapsedtime)
+    print("Comparaciones", values[1])
+    print('Intercambios', values[2])
     return values
